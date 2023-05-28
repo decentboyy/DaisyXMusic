83ee6b1318f813efe025576dcf9374a540d6e2e2
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-29 02:21:42 +0530

    Update __main__.py

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import Client as Bot
 from pytgcalls import idle
 
-from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from DaisyXMusic.config import  API_ID, API_HASH, BG_IMAGE, BOT_TOKEN
 from DaisyXMusic.services.pytgcalls import run
 
 response = requests.get(BG_IMAGE)
